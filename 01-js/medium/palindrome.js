7a952483ff298fbef803930da53c5cf458c3647f
5atvik <143879267+5atvik@users.noreply.github.com>
2024-01-18 18:00:00 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,18 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
+function isPalindrome(str){
+  let v= str.split("");
+  let a= str[0];
+  for(let i=0; i<str.length; i++){
+    if(a==str[i]){
+      return true;
+    }else {
+      return false;
+    }
+  }
 }
+let str= "nan";
+console.log(isPalindrome(str));
 
 module.exports = isPalindrome;
